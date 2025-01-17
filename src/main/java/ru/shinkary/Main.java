c69b2f8c99d1fe6f9d36d87f308018143646a493
Ekaterina Chebotaeva <ek.chebotaeva@yandex.ru>
2025-01-17 21:04:28 +0500

    i need conflict, so...

--- src/main/java/ru/shinkary/Main.java
+++ src/main/java/ru/shinkary/Main.java
@@ -2,7 +2,7 @@ package ru.shinkary;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("No!");
+        System.out.println("oh, i can't changed the branch 1 without changed branch 2 on local, right?");
     }
 
     private static String getMessage() {
